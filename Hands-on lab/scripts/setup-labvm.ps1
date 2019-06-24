30a7dd2911c462c25f2c70403a9080b795d4ab42
crpietschmann <chris@buildazure.com>
2019-06-24 16:24:42 -0500

    fix

--- Hands-on lab/scripts/setup-labvm.ps1
+++ Hands-on lab/scripts/setup-labvm.ps1
@@ -8,19 +8,20 @@ choco install sql-server-management-studio -y
 $labFilesFolder = "C:\LabFiles"
 if ([string]::IsNullOrEmpty($labFilesUri) -eq $false)
 {
-    # Ensure folder exists
+    Write-Host "Make sure folder exists"
     if ((Test-Path $labFilesFolder) -eq $false)
     {
         New-Item -Path $labFilesFolder -ItemType directory
     }
 
-    # Parse file name
+    Write-Host "Parse file name"
     $filePathParts = $labFilesUri.Split("/")
     $fileName = $filePathParts[$filePathParts.Length - 1]
+    Write-Host "File Name: $fileName"
 
-    # Download .ZIP file
+    Write-Host "Download File..."
     (New-Object System.Net.WebClient).DownloadFile($labFilesUri, $labFilesFolder)
 
-    # Extract .ZIP file
+    Write-Host "Extract .ZIP file..."
     Expand-Archive -Path $fileName -DestinationPath $labFilesFolder -Force
 }
